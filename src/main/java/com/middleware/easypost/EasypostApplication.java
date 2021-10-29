193ade93ef8e878af982a3d5ba39328d4ef48344
Carlos Augusto <caragon@summasolutions.net>
2021-10-28 21:40:08 -0500

    Blank project

---
+++ src/main/java/com/middleware/easypost/EasypostApplication.java
@@ -0,0 +1,13 @@
+package com.middleware.easypost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasypostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasypostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/middleware/easypost/EasypostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middleware.easypost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasypostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
