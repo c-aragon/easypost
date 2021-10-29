76b3cd119e8f142152b5964be20510c379095d19
Oduad <aragon_1123@hotmail.com>
2021-10-29 13:23:22 -0500

    Created an implementation for Controller.

---
+++ src/main/java/com/middleware/easypost/controllerImp/OrderControllerImp.java
@@ -0,0 +1,19 @@
+package com.middleware.easypost.controllerImp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.middleware.easypost.controller.OrderController;
+import com.middleware.easypost.dto.OrderDTO;
+
+@RestController
+@RequestMapping(path="order")
+public class OrderControllerImp implements OrderController{
+
+	@Override
+	public OrderDTO handleOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
