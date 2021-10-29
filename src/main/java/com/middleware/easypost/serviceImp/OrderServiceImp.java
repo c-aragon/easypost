21a243956cb48cbccf781941666a19ba5ae0fcc2
Oduad <aragon_1123@hotmail.com>
2021-10-29 13:30:12 -0500

    Created an Implementation for Service.

---
+++ src/main/java/com/middleware/easypost/serviceImp/OrderServiceImp.java
@@ -0,0 +1,15 @@
+package com.middleware.easypost.serviceImp;
+import org.springframework.stereotype.Service;
+import com.middleware.easypost.dto.OrderDTO;
+import com.middleware.easypost.service.OrderService;
+
+@Service
+public class OrderServiceImp implements OrderService {
+
+	@Override
+	public OrderDTO handleOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
