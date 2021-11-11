83903ea5ea0464c001eda4e7d4786a47b7fb842e
Oduad <aragon_1123@hotmail.com>
2021-11-11 16:05:11 -0600

    refactor

--- src/main/java/com/middleware/easypost/controller/OrderController.java
+++ src/main/java/com/middleware/easypost/controller/OrderController.java
@@ -1,12 +1,7 @@
 package com.middleware.easypost.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.middleware.easypost.dto.OrderDTO;
-
 public interface OrderController {
 
-	OrderDTO handleOrder();
+	com.summa.sellercenter.dto.order.OrderDTO handleOrder();
 	
 }
